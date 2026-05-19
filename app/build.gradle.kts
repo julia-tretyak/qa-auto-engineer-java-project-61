3dd1745177562ed044c402294b66258f697781e1
Юлия Третьяк <julia@tretyak.com>
2026-05-19 16:48:21 +0300

    Add SonarQube GitHub Actions workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     application
+    checkstyle
+    id("org.sonarqube") version "5.1.0.4882"
 }
 
 group = "hexlet.code"
@@ -12,6 +14,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    checkstyle("com.puppycrawl.tools:checkstyle:10.12.0")
 }
 
 tasks.test {
@@ -20,4 +23,13 @@ tasks.test {
 
 application {
     mainClass = "hexlet.code.App"
+}
+
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
+
+checkstyle {
+    toolVersion = "10.12.0"
+    configFile = file("${project.rootDir}/config/checkstyle/checkstyle.xml")
 }
